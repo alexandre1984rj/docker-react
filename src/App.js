b96018fa66292e97e1ddad1dee1b359160d1402a
Alexandre <alexandre@MacBook-Pro-de-Alexandre.local>
2019-10-12 15:05:42 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+		  Testando 123456789 e os hexadecimais ABCDE
         </p>
         <a
           className="App-link"
